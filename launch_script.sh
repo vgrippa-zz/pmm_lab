0ff619ccf7acd439afb2dd92a39838f3b99d9014
vgrippa <vgrippa@gmail.com>
2019-09-22 13:43:40 +0300

    Setting PMM2 config

--- launch_script.sh
+++ launch_script.sh
@@ -2,16 +2,16 @@
 vagrant up
 
 # Setting PMM
-ansible-playbook provision/playbooks/playbook_pmm.yml
+ansible-playbook provision/playbook_pmm.yml
 
 # Setting MongoDB
-ansible-playbook provision/playbooks/playbook_mongo.yml -i provision/hosts  --key-file /root/.vagrant.d/insecure_private_key
+ansible-playbook provision/playbook_mongo.yml -i provision/hosts  --key-file /root/.vagrant.d/insecure_private_key
 
 # Setting MySQL
-ansible-playbook provision/playbooks/playbook_mysql.yml -i provision/hosts  --key-file /root/.vagrant.d/insecure_private_key
+ansible-playbook provision/playbook_mysql.yml -i provision/hosts  --key-file /root/.vagrant.d/insecure_private_key
 
 # Setting Postgres
-ansible-playbook provision/playbooks/playbook_postgres.yml -i provision/hosts  --key-file /root/.vagrant.d/insecure_private_key
+ansible-playbook provision/playbook_postgres.yml -i provision/hosts  --key-file /root/.vagrant.d/insecure_private_key
 
 # Setting App Server
-ansible-playbook provision/playbooks/playbook_extra.yml -i provision/hosts  --key-file /root/.vagrant.d/insecure_private_key
+ansible-playbook provision/playbook_extra.yml -i provision/hosts  --key-file /root/.vagrant.d/insecure_private_key
