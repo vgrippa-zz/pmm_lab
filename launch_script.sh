c88c9013d623aa8591a98cdf1413666d3c49a980
vgrippa <vgrippa@gmail.com>
2019-09-23 15:03:19 +0300

    Setting PMM2 config

--- launch_script.sh
+++ launch_script.sh
@@ -2,16 +2,16 @@
 vagrant up
 
 # Setting PMM
-nohup ansible-playbook provision/playbook_pmm.yml &
+(ansible-playbook provision/playbook_pmm.yml ) &
 
 # Setting MongoDB
-nohup sleep 94; ansible-playbook provision/playbook_mongo.yml -i provision/hosts  --key-file /root/.vagrant.d/insecure_private_key &
+(ansible-playbook provision/playbook_mongo.yml -i provision/hosts  --key-file /root/.vagrant.d/insecure_private_key ) &
 
 # Setting MySQL
-nohup sleep 63; ansible-playbook provision/playbook_mysql.yml -i provision/hosts  --key-file /root/.vagrant.d/insecure_private_key &
+(ansible-playbook provision/playbook_mysql.yml -i provision/hosts  --key-file /root/.vagrant.d/insecure_private_key ) &
 
 # Setting Postgres
-nohup sleep 55; ansible-playbook provision/playbook_postgres.yml -i provision/hosts  --key-file /root/.vagrant.d/insecure_private_key &
+(ansible-playbook provision/playbook_postgres.yml -i provision/hosts  --key-file /root/.vagrant.d/insecure_private_key ) &
 
 # Setting App Server
 # Sleeping a few minutes to wait for MySQL and Postgres get ready
