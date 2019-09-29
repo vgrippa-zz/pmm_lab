b56708f0ebfe7653ee81730ed5e698c28c4a0945
vgrippa <vgrippa@gmail.com>
2019-09-29 17:15:21 +0200

    minikube setup

--- minikube_setup.sh
+++ minikube_setup.sh
@@ -1,7 +1,6 @@
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
-  && chmod +x minikube
-
-sudo install minikube /usr/local/bin/
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64  \
+  && install minikube-linux-amd64 /usr/local/bin/minikube \
+  && minikube version
 
 
 curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
